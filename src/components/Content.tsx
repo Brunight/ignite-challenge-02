b83e02f1448b206e93fc1e3d513df6c505e5ac86
Bruno Rodrigues <brunorodriguesricardo@gmail.com>
2021-03-15 12:15:18 -0300

    Challenge solved 🎉

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,37 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from '../components/MovieCard';
+
+import '../styles/content.scss';
+
+interface MovieProps {
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface Props {
+  movies: MovieProps[];
+  genreTitle: string;
+}
+
+export const Content: React.FC<Props> = ({ movies, genreTitle }) => {
+  return (
+
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {genreTitle}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
